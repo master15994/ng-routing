7002e8c99025771546c7f88fde598b05a3ab40d3
Askar Pulatov <master15994@gmail.com>
2023-05-06 13:37:33 +0300

    8. Работа с параметрами

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../post.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  showIds = false;
+
   get post() {
     return this.postsService.posts;
   }
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute,
+    public router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.showIds = params['showIds'];
+    });
+  }
+
+  showIdProgram() {
+    this.router.navigate(['/posts'], { queryParams: { showIds: true } });
+  }
 }
